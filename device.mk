7ac77bcca2b918ca1d1307a2adcb3acedfda7d5b
miloxx <milosz.paszkowski@web.de>
2019-08-16 13:54:40 +0200

    updated device.mk for twrp

--- device.mk
+++ device.mk
@@ -1,137 +1,22 @@
-#
-# Copyright (C) 2014 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+LOCAL_PATH := device/zte/kis3
 
-DEVICE_PACKAGE_OVERLAYS += device/zte/kis3/overlay
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
-$(call inherit-product-if-exists, vendor/zte/kis3/kis3-vendor.mk)
-
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-
-# Audio
-PRODUCT_PACKAGES += \
-    audio.primary.msm8610 \
-    audio.a2dp.default \
-    audio.r_submix.default \
-    audio.usb.default \
-    libaudio-resampler \
-    libqcomvisualizer \
-    libqcomvoiceprocessing
-
-# Camera
-PRODUCT_PACKAGES += \
-    camera.msm8610 \
-    Snap
-
-# Compatibility
-PRODUCT_PACKAGES += \
-    libboringssl-compat \
-    libshim_qc-opt \
-    libshim_rmt_storage
-
-# Display
-PRODUCT_PACKAGES += \
-    copybit.msm8610 \
-    gralloc.msm8610 \
-    hwcomposer.msm8610 \
-    memtrack.msm8610
-
-# Doze
-PRODUCT_PACKAGES += \
-    DozeService
-
-# Ebtables
-PRODUCT_PACKAGES += \
-    ebtables \
-    ethertypes \
-    libebtc
-
-# FM radio
-PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
-
-# GPS
-PRODUCT_PACKAGES += \
-    gps.msm8610
-
-# Keystore
-PRODUCT_PACKAGES += \
-    keystore.msm8610
-
-# Lights
-PRODUCT_PACKAGES += \
-    lights.msm8610
-
-# Misc
-PRODUCT_PACKAGES += \
-    libxml2
-
-# OMX
-PRODUCT_PACKAGES += \
-    libextmedia_jni \
-    libOmxCore \
-    libOmxVdec \
-    libOmxVenc \
-    libstagefrighthw
-
-# Power
-PRODUCT_PACKAGES += \
-    power.msm8610
-
-# Wifi
-PRODUCT_PACKAGES += \
-    hostapd \
-    wpa_supplicant
-
-PRODUCT_PACKAGES += \
-    libQWiFiSoftApCfg \
-    libwcnss_qmi \
-    wcnss_service
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
 
 PRODUCT_COPY_FILES += \
-    kernel/zte/msm8610/drivers/staging/prima/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    kernel/zte/msm8610/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/recovery/fstab.qcom:root/fstab.qcom \
+    $(LOCAL_PATH)/recovery/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
+    $(LOCAL_PATH)/recovery/ueventd.qcom.rc:root/ueventd.qcom.rc
 
-# Ramdisk
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,device/zte/kis3/ramdisk,root)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/verity.mk)
 
-# Prebuilt
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,device/zte/kis3/prebuilt/system,system)
+ifneq (,$(filter eng,$(TARGET_BUILD_VARIANT)))
+	PRODUCT_SUPPORTS_BOOT_SIGNER := true
+	PRODUCT_VERITY_SIGNING_KEY := build/target/product/security/verity
+endif
 
-# Media codecs
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+PRODUCT_NAME := kis3
