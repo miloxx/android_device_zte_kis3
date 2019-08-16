f1396690a1dd2afdc1a24b02bb06cc0908dd9cd1
miloxx <milosz.paszkowski@web.de>
2019-08-16 15:00:14 +0200

    changed from lineage to omni for twrp

--- omni_kis3.mk
+++ omni_kis3.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kis3
-PRODUCT_NAME := lineage_kis3
+PRODUCT_NAME := omni_kis3
 PRODUCT_BRAND := ZTE
 PRODUCT_MODEL := Kis 3
 PRODUCT_MANUFACTURER := ZTE
